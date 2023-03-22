db3c18d1a2124ae677eb3665e1e125284c919c16
Filippos <phil.pidis@gmail.com>
2023-03-22 16:21:40 +0200

    objects commit 1

---
+++ 04_Objects/index.js
@@ -0,0 +1,41 @@
+// 01
+
+// variables
+// let radius = 1;
+// let x = 1;
+// let y = 1;
+
+// vs
+// object
+
+// (OOP) Object Oriented Programming
+
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1,
+//   },
+//   isVisible: true,
+//   draw: function () {
+//     console.log("Hi");
+//   },
+// };
+
+// circle.draw();
+
+// 03
+
+// Construction Function
+// USe Pascal notation for these
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const circle = new Circle(1);
+
+// Objects are dynamic, you can add and remove properties and methods
