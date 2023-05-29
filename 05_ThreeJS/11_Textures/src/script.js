0dd8d6d3a5491cbb0a07636f1482513d096ad7fb
Filippos <phil.pidis@gmail.com>
2023-05-29 11:30:49 +0300

    threeJS textures final commit

--- 05_ThreeJS/11_Textures/src/script.js
+++ 05_ThreeJS/11_Textures/src/script.js
@@ -3,6 +3,9 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // Textures
 
+// Note: if you add an image in the src folder you can import imageSource from './filename.jpg'
+// Note: if you add an image in the static folder you can preview it by changing the url and adding the filename.jpg
+
 // Version 1
 // const img = new Image();
 // const texture = new THREE.Texture(img);
@@ -15,24 +18,29 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 // You only need one texture loader to add multiple textures
 const loadingManager = new THREE.LoadingManager();
 
-loadingManager.onStart = () => {
-  console.log("onStart");
-};
+// Call back Functions to monitor image loading, models fonts. It monitors the progress
+// of the loading of these assets hence the loading manager
+// loadingManager.onStart = () => {
+//   console.log("onStart");
+// };
 
-loadingManager.onLoad = () => {
-  console.log("onLoad");
-};
+// loadingManager.onLoad = () => {
+//   console.log("onLoad");
+// };
 
-loadingManager.onProgress = () => {
-  console.log("onProgress");
-};
+// loadingManager.onProgress = () => {
+//   console.log("onProgress");
+// };
 
-loadingManager.onError = () => {
-  console.log("onError");
-};
+// loadingManager.onError = () => {
+//   console.log("onError");
+// };
 
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const colorTexture = textureLoader.load("/textures/door/color.jpg");
+// const colorTexture = textureLoader.load("/textures/door/color.jpg");
+//const colorTexture = textureLoader.load("/textures/checkerboard-1024x1024.png");
+//const colorTexture = textureLoader.load("/textures/checkerboard-8x8.png");
+const colorTexture = textureLoader.load("/textures/minecraft.png");
 const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
 const heightTexture = textureLoader.load("/textures/door/height.jpg");
 const normalTexture = textureLoader.load("/textures/door/normal.jpg");
@@ -41,6 +49,32 @@ const ambientTexture = textureLoader.load(
 );
 const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
 const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+
+// Transformations
+
+// colorTexture.repeat.x = 2;
+// colorTexture.repeat.y = 3;
+// colorTexture.wrapS = THREE.RepeatWrapping;
+// colorTexture.wrapT = THREE.RepeatWrapping;
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping;
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping;
+
+// colorTexture.offset.x = 0.5;
+// colorTexture.offset.y = 0.5;
+
+// colorTexture.rotation = Math.PI * 0.25;
+// colorTexture.center.x = 0.5;
+// colorTexture.center.y = 0.5;
+
+// Filtering & Midmapping
+
+// Minification filter - minFilter property
+
+//colorTexture.generateMipmaps = true;
+//colorTexture.minFilter = THREE.NearestFilter;
+colorTexture.magFilter = THREE.NearestFilter;
+
+// Texture Format & Optimization
 
 // const colorTexture = textureLoader.load(
 //   "/textures/door/color.jpg",
@@ -68,8 +102,9 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-//const material = new THREE.MeshBasicMaterial({ map: colorTexture });
-const material = new THREE.MeshBasicMaterial({ map: normalTexture });
+console.log(geometry.attributes);
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
+// const material = new THREE.MeshBasicMaterial({ map: normalTexture });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
